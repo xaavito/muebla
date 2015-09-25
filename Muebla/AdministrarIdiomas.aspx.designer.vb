063f7e39b5597b712bc0172e2af515416928c92c
Javier Gonzalez <javiermartingonzalez@gmail.com>
2015-09-25 17:21:53 -0300

    Manejador de idiomas

--- Muebla/AdministrarIdiomas.aspx.designer.vb
+++ Muebla/AdministrarIdiomas.aspx.designer.vb
@@ -41,15 +41,6 @@ Partial Public Class AdministrarIdiomas
     Protected WithEvents idiomaDropDownList As Global.System.Web.UI.WebControls.DropDownList
 
     '''<summary>
-    '''Control buscarButton.
-    '''</summary>
-    '''<remarks>
-    '''Campo generado automáticamente.
-    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-    '''</remarks>
-    Protected WithEvents buscarButton As Global.System.Web.UI.WebControls.Button
-
-    '''<summary>
     '''Control idiomaResultadosDataGrid.
     '''</summary>
     '''<remarks>
@@ -57,4 +48,49 @@ Partial Public Class AdministrarIdiomas
     '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
     '''</remarks>
     Protected WithEvents idiomaResultadosDataGrid As Global.System.Web.UI.WebControls.GridView
+
+    '''<summary>
+    '''Control editDiv.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents editDiv As Global.System.Web.UI.HtmlControls.HtmlGenericControl
+
+    '''<summary>
+    '''Control tableEditIdioma.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents tableEditIdioma As Global.System.Web.UI.WebControls.Table
+
+    '''<summary>
+    '''Control textoLabel.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents textoLabel As Global.System.Web.UI.WebControls.Label
+
+    '''<summary>
+    '''Control textoTextBox.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents textoTextBox As Global.System.Web.UI.WebControls.TextBox
+
+    '''<summary>
+    '''Control confirmarButton.
+    '''</summary>
+    '''<remarks>
+    '''Campo generado automáticamente.
+    '''Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+    '''</remarks>
+    Protected WithEvents confirmarButton As Global.System.Web.UI.WebControls.Button
 End Class
--- Muebla/AdministrarIdiomas.aspx.vb
+++ Muebla/AdministrarIdiomas.aspx.vb
@@ -2,11 +2,21 @@
     Inherits ExtendedPage
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        If Page.IsPostBack Then
+            Return
+        End If
+        If Session("idiomaChange") Is Nothing Then
+            Me.idiomaDropDownList.DataSource = BLL.GestorIdiomaBLL.buscarIdiomas
+            Me.idiomaDropDownList.DataTextField = "descripcion"
+            Me.idiomaDropDownList.DataValueField = "id"
+            Me.idiomaDropDownList.DataBind()
 
-    End Sub
-
-    Protected Sub buscarButton_Click(sender As Object, e As EventArgs)
-
+            Dim idioma As New BE.IdiomaBE
+            idioma.id = 1
+            Me.idiomaResultadosDataGrid.DataSource = BLL.GestorIdiomaBLL.buscarComponentes(idioma)
+            Me.idiomaResultadosDataGrid.DataBind()
+        End If
+        Me.editDiv.Visible = False
     End Sub
 
     Protected Sub idiomaResultadosDataGrid_PreRender(sender As Object, e As EventArgs)
@@ -14,6 +24,26 @@
     End Sub
 
     Protected Sub idiomaDropDownList_SelectedIndexChanged(sender As Object, e As EventArgs)
+        Try
+            Dim idioma As New BE.IdiomaBE
+            Session("idiomaChange") = Me.idiomaDropDownList.SelectedValue
+            idioma.id = Me.idiomaDropDownList.SelectedValue
+            Me.idiomaResultadosDataGrid.DataSource = BLL.GestorIdiomaBLL.buscarComponentes(idioma)
+            Me.idiomaResultadosDataGrid.DataBind()
+        Catch ex As Exception
+            logMessage(ex)
+        End Try
+    End Sub
 
+    Protected Sub confirmarButton_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Protected Sub ibtnEdit_Click(sender As Object, e As ImageClickEventArgs)
+        Dim gvRow As GridViewRow = CType(CType(sender, ImageButton).NamingContainer, GridViewRow)
+        Dim con As Label = CType(Me.idiomaResultadosDataGrid.Rows(gvRow.RowIndex).Cells(0).FindControl("itemID"), Label)
+        Dim id As Integer = Integer.Parse(con.Text.ToString)
+        Session("idTextoIdioma") = id
+        Me.editDiv.Visible = True
     End Sub
 End Class
